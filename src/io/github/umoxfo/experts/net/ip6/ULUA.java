4bbb9cc8684189cac7c63eccea162bd6835f81f2
Umoxfo <ycco34vx@gmail.com>
2017-09-09 15:52:02 -0700

    Updated Copyright

--- src/io/github/umoxfo/experts/net/ip6/ULUA.java
+++ src/io/github/umoxfo/experts/net/ip6/ULUA.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2017. Makoto Sakaguchi
- * This file is part of Network.
+ * This file is part of Experts Net.
  *
- * Network is free software: you can redistribute it and/or modify
+ * Experts Net is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Network is distributed in the hope that it will be useful,
+ * Experts Net is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Experts Net.  If not, see <http://www.gnu.org/licenses/>.
  */
 package io.github.umoxfo.experts.net.ip6;
 
