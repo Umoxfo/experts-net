2aa81414e6910e8f40ae4a60dcb74a6bb4f85369
Umoxfo <ycco34vx@gmail.com>
2017-10-03 19:07:23 -0700

    Add test class (sitll empty)

---
+++ src/test/java/io/github/umoxfo/experts/net/subnet/SubnetUtilsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017. Makoto Sakaguchi
+ * This file is part of Experts Net.
+ *
+ * Experts Net is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Experts Net is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.umoxfo.experts.net.subnet;
+
+import org.junit.jupiter.api.Test;
+
+class SubnetUtilsTest {
+	@Test
+	void getByCIDRNotation() {
+	}
+
+	@Test
+	void getByMask() {
+	}
+
+	@Test
+	void toCIDR() {
+	}
+
+	@Test
+	void toMask() {
+	}
+
+	@Test
+	void getHostCount() {
+	}
+
+}
